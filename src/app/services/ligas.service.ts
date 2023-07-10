c0906bc13a1c158ad404ff141dfce0f4ef0da0d6
Alan Roy Aupart Vargas <alan.aupart.vargas@kairosds.com>
2023-07-10 14:11:43 -0600

    Se agregan servicios para Ligas

--- src/app/services/ligas.service.ts
+++ src/app/services/ligas.service.ts
@@ -3,9 +3,55 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class LigasService {
+  public ctxVaronil = 'http://localhost:8000/msvc-liga/liga-varonil/api';
+  public ctxFemenil = 'http://localhost:8000/msvc-liga/liga-femenil/api';
 
   constructor(private http: HttpClient) {}
+
+  validarContexto(liga: string): string {
+    if (liga === 'Varonil') {
+      return this.ctxVaronil;
+    } else {
+      return this.ctxFemenil;
+    }
+  }
+
+  nueva_liga(liga: string, request: any): Observable<any> {
+    return this.http.post(this.validarContexto(liga) + '/nueva-liga', request);
+  }
+
+  consultar_liga(liga: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/consultar-ligas');
+  }
+
+  agregar_equipo_liga(liga: string, idLiga: string, idEquipo: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-equipo-liga/' + idLiga + '/' + idEquipo);
+  }
+
+  agregar_partido_liga(liga: string, idLiga: string, idPartido: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-partido-liga/' + idLiga + '/' + idPartido);
+  }
+
+  agregar_tabla_liga(liga: string, idLiga: string, idTabla: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-tabla-liga/' + idLiga + '/' + idTabla);
+  }
+
+  agregar_resultado_liga(liga: string, idLiga: string, idResultado: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-resultado-liga/' + idLiga + '/' + idResultado);
+  }
+
+  agregar_goles_liga(liga: string, idLiga: string, idGoles: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-goles-liga/' + idLiga + '/' + idGoles);
+  }
+
+  agregar_tarjetas_liga(liga: string, idLiga: string, idTarjetas: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-tarjetas-liga/' + idLiga + '/' + idTarjetas);
+  }
+
+  agregar_mvp_liga(liga: string, idLiga: string, idMvp: string): Observable<any> {
+    return this.http.get(this.validarContexto(liga) + '/agregar-mvp-liga/' + idLiga + '/' + idMvp);
+  }
 }
